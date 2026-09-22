a80248db3196e75719ce6ca45eb402c8457167e0
shehrozali6465372-ctrl <shehrozali6465372@gmail.com>
2026-09-22 06:14:17 +0500

    Scope analytics hook tests to explicit account

--- tests/layer07_publishing/test_analytics_hook.py
+++ tests/layer07_publishing/test_analytics_hook.py
@@ -540,7 +540,7 @@ class TestAnalyticsManager:
         }
 
     def test_analyze_post(self):
-        report = self.mgr.analyze_post("facebook", "p1", self._fetcher)
+        report = self.mgr.analyze_post("facebook", "p1", self._fetcher, account_id="acct_test")
         assert report is not None
         assert report.platform == "facebook"
         assert "engagement" in report.to_dict()
@@ -559,7 +559,7 @@ class TestAnalyticsManager:
 
     def test_get_reports(self):
         self.mgr.analyze_post("facebook", "p1", self._fetcher)
-        self.mgr.analyze_post("linkedin", "p2", self._fetcher)
+        self.mgr.analyze_post("linkedin", "p2", self._fetcher, account_id="acct_test")
         all_reports = self.mgr.get_reports()
         assert len(all_reports) == 2
         fb_reports = self.mgr.get_reports("facebook")
