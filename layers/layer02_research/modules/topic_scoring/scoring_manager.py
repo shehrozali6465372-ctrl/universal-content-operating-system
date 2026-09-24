6975b3eb1041dca49a29ce995ef7e8779e8880cf
shehrozali6465372-ctrl <shehrozali6465372@gmail.com>
2026-09-25 04:29:25 +0500

    fix(layer2): use reentrant locks in research managers

--- layers/layer02_research/modules/topic_scoring/scoring_manager.py
+++ layers/layer02_research/modules/topic_scoring/scoring_manager.py
@@ -27,7 +27,7 @@ class ScoringManager:
 
     def __init__(self, storage_path: Optional[str] = None):
         self._results: Dict[str, ScoringResult] = {}
-        self._lock = Lock()
+        self._lock = RLock()
         self._storage_path = Path(storage_path) if storage_path else None
 
         self.engine = ScoringEngine()
