5cacbe29bdf1144ffcf104ad4195842037612caf
shehrozali6465372-ctrl <shehrozali6465372@gmail.com>
2026-09-26 03:54:53 +0500

    Fix Layer 11 event loop lint violations

--- layers/layer11_async_runtime/modules/event_loop_management/exceptions.py
+++ layers/layer11_async_runtime/modules/event_loop_management/exceptions.py
@@ -1,7 +1,11 @@
 """Exceptions for Event Loop Management."""
 from __future__ import annotations
 
-class EventLoopError(Exception): pass
-class LoopTimeoutError(EventLoopError): pass
-class LoopRecoveryError(EventLoopError): pass
-class LoopBalancingError(EventLoopError): pass
+class EventLoopError(Exception):
+    pass
+class LoopTimeoutError(EventLoopError):
+    pass
+class LoopRecoveryError(EventLoopError):
+    pass
+class LoopBalancingError(EventLoopError):
+    pass
