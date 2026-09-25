cfd78eb7103c96b67c876fe662f4e7b5aa194abc
shehrozali6465372-ctrl <shehrozali6465372@gmail.com>
2026-09-26 03:54:11 +0500

    Fix Layer 11 parallel lint violations

--- layers/layer11_async_runtime/modules/concurrent_execution/parallel_strategy.py
+++ layers/layer11_async_runtime/modules/concurrent_execution/parallel_strategy.py
@@ -7,6 +7,7 @@ class Parallelstrategy:
         self._data: List[Dict[str, Any]] = []
     def record(self, data: Dict[str, Any]) -> None:
         self._data.append(data)
-        if len(self._data) > 500: self._data = self._data[-500:]
+        if len(self._data) > 500:
+            self._data = self._data[-500:]
     def get_stats(self) -> Dict[str, Any]:
         return {"total": len(self._data)}
