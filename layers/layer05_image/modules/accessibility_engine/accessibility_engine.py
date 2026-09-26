7019bfa9312a09548411ea6ee3a02584e30f047a
shehrozali6465372-ctrl <shehrozali6465372@gmail.com>
2026-09-26 06:00:54 +0500

    fix Layer 5 accessibility counter race

--- layers/layer05_image/modules/accessibility_engine/accessibility_engine.py
+++ layers/layer05_image/modules/accessibility_engine/accessibility_engine.py
@@ -59,6 +59,7 @@ class AccessibilityEngine:
             result.score -= 20
         result.recommendations.append("Ensure text is at least 24pt")
         with self._counter_lock:
+            with self._counter_lock:
             self._check_count += 1
         return result
 
